d3ef2426592f37cc7da1e7e2cf5fe49aa9d77de5
Mar-er <243958407@qq.com>
2020-05-29 12:40:11 +0800

    chore: 增加redis配置

--- src/core/setup/express.ts
+++ src/core/setup/express.ts
@@ -2,23 +2,23 @@ import { NestExpressApplication } from '@nestjs/platform-express'
 import { ConfigService } from '@nestjs/config'
 // import { Request, Response, NextFunction } from 'express';
 // import * as passport from 'passport';
-// import * as cookieParser from 'cookie-parser';
-// import * as RedisClient from 'ioredis';
-// import * as expressSession from 'express-session';
-// import * as connectRedis from 'connect-redis';
+import * as cookieParser from 'cookie-parser'
+import * as RedisClient from 'ioredis'
+import * as expressSession from 'express-session'
+import * as connectRedis from 'connect-redis'
 // import * as csurf from 'csurf';
 // import * as ejsMate from 'ejs-mate';
 // import * as loaderConnect from 'loader-connect';
 // import * as flash from 'connect-flash';
-import { EnvironmentConfig } from '../config'
-import { join, resolve } from 'path'
+import { EnvironmentConfig, RedisConfig } from '../config'
+// import { join, resolve } from 'path'
 
 export default (app: NestExpressApplication) => {
   const configService = app.get(ConfigService)
 
   const environment = configService.get<EnvironmentConfig>('environment')
-  const dir = environment.isDevelopment ? 'src' : 'dist'
-  const rootDir = resolve(process.cwd(), dir)
+  // const dir = environment.isDevelopment ? 'src' : 'dist'
+  // const rootDir = resolve(process.cwd(), dir)
 
   // // 注意：这个要在express.static之前调用，loader2.0之后要使用loader-connect
   // // 自动转换less为css
@@ -38,19 +38,19 @@ export default (app: NestExpressApplication) => {
   // app.setBaseViewsDir(join(rootDir, 'views'));
 
   // // 链接Redis
-  // const RedisStore = connectRedis(expressSession);
-  // const secret = environment.session_secret;
-  // // 注册session中间件
-  // app.use(expressSession({
-  //     name: 'jiayi',
-  //     secret,  // 用来对 session id 相关的 cookie 进行签名
-  //     store: new RedisStore({ client: new RedisClient(configService.get<RedisConfig>('redis')) }),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
-  //     saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
-  //     resave: false,  // 是否每次都重新保存会话，建议false
-  // }));
+  const RedisStore = connectRedis(expressSession)
+  const secret = environment.session_secret
+  // 注册session中间件
+  app.use(expressSession({
+    name: 'jiayi',
+    secret, // 用来对 session id 相关的 cookie 进行签名
+    store: new RedisStore({ client: new RedisClient(configService.get<RedisConfig>('redis')) }), // 本地存储session（文本文件，也可以选择其他store，比如redis的）
+    saveUninitialized: false, // 是否自动保存未初始化的会话，建议false
+    resave: false // 是否每次都重新保存会话，建议false
+  }))
 
-  // // 注册cookies中间件
-  // app.use(cookieParser(secret));
+  // 注册cookies中间件
+  app.use(cookieParser(secret))
 
   // // 注册passport中间件
   // app.use(passport.initialize());
